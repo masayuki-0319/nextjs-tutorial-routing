bb2979f385d1ba3d564834d7598ae03665038011
将之大西 <omccello@gmail.com>
2021-10-24 12:25:11 +0900

    lec63

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -1,7 +1,23 @@
+import { useRouter } from 'next/router';
+
 const Show = () => {
+  const router = useRouter();
+
+  console.log(router.query);
+  const loadProjectHandler = () => {
+    router.push({
+      pathname: '/clients/[id]/[projectid]',
+      query: {
+        id: router.query?.id,
+        projectid: 'Project_A',
+      },
+    });
+  };
+
   return (
     <div>
       <h1>Clients Show Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
