b21bb5cff8b55f1434c8e169d39b4f2f637d1e00
将之大西 <omccello@gmail.com>
2021-10-24 11:42:55 +0900

    lec60

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
-const Index = () => {
+import Link from 'next/link';
+
+const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <ul>
+        <li>
+          <Link href='/portfolio'>Portfolio Page</Link>
+        </li>
+        <li>
+          <Link href='/clients'>Clients Page</Link>
+        </li>
+        <li>
+          <Link href='/blog/2021/01/01/name'>Blog Page</Link>
+        </li>
+      </ul>
     </div>
   );
 };
 
-export default Index;
+export default HomePage;
