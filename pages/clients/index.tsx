5708bfe2230883e22477206ff6c9302b705cbd54
将之大西 <omccello@gmail.com>
2021-10-24 12:15:40 +0900

    lec61

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+
 const Index = () => {
+  const clients = [
+    { id: 'max', name: 'Maximilian' },
+    { id: 'masayuki', name: 'Masayuki' },
+  ];
+
   return (
     <div>
       <h1>Clients Index Page</h1>
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
